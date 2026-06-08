7e4269b1a0fdd8399de07ae629cc27a725aeb891
Luis356 <luisricardouser@gmail.com>
2026-06-08 19:03:09 -0300

    Feat: melhoria no cadastro

--- cypress/e2e/Cadastro.cy.js
+++ cypress/e2e/Cadastro.cy.js
@@ -1,14 +1,12 @@
 describe('Cadastro de usuário', () => {
   it('Cadastro de usuário regular com sucesso', () => {
     cy.visit('/cadastrarusuarios')
-    cy.campos('Teste_cadastro_regular', 'testecadastroregular@example.com', 'senha123', false)
+    cy.campos('Victorino Salina', 'victorino.salina@example.com', 'senha123', false)
     cy.get('[data-testid="cadastrar"]').click()
 
     cy.contains('Cadastro realizado com sucesso').should('be.visible')
-
-    cy.deleteUsuario('testecadastroregular@example.com')
   })
-
+  
   it('Cadastro de usuário administrador com sucesso', () => {
     cy.visit('/cadastrarusuarios')
     cy.campos('Teste_cadastro_admin', 'testecadastroadmin@example.com', 'senha123', true)
@@ -23,7 +21,7 @@ describe('Cadastro de usuário', () => {
     cy.intercept('POST', '**/usuarios').as('cadastroSemCredenciais')
     cy.visit('/cadastrarusuarios')
     cy.get('[data-testid="cadastrar"]').click()
-
+    
     cy.contains('Nome é obrigatório').should('be.visible')
     cy.contains('Email é obrigatório').should('be.visible')
     cy.contains('Password é obrigatório').should('be.visible')
@@ -31,13 +29,13 @@ describe('Cadastro de usuário', () => {
   })
 
   it('Cadastro sem sucesso - usuário já cadastrado', () => {
-    cy.apiCadastro('Victorino Salina', 'victorino.salina@example.com', 'senha123', true)
     cy.intercept('POST', '**/usuarios').as('usuarioExistente')
     cy.visit('/cadastrarusuarios')
-    cy.campos('Victorino Salina', 'victorino.salina@example.com', 'senha123', true)
+    cy.campos('Victorino Salina', 'victorino.salina@example.com', 'senha123', false)
     cy.get('[data-testid="cadastrar"]').click()
 
     cy.contains('Este email já está sendo usado').should('be.visible')
     cy.wait('@usuarioExistente').its('response.statusCode').should('eq', 400)
+    cy.deleteUsuario('victorino.salina@example.com')
   })
 })
